27ef5b817b36d63beac59ab96d0bcf8c20a03407
Luckysting <work.romanuel@gmail.com>
2020-02-09 18:19:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
